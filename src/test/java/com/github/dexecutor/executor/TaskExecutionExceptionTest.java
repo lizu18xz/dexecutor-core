e87447c320b4fb38396b18bcba933407b4d305fc
Nadeem Mohammad <coolmind182006@gmail.com>
2016-08-24 13:22:46 +0530

    More tests

---
+++ src/test/java/com/github/dexecutor/executor/TaskExecutionExceptionTest.java
@@ -0,0 +1,23 @@
+package com.github.dexecutor.executor;
+
+import static org.junit.Assert.*;
+import static org.hamcrest.CoreMatchers.*;
+
+import org.junit.Test;
+
+public class TaskExecutionExceptionTest {
+
+	@Test
+	public void testMsgShouldBeAsPassed() {
+		String msg = "MSG";
+		TaskExecutionException ex = new TaskExecutionException(msg);
+		assertThat(ex.getMessage(), equalTo(msg));
+	}
+	
+	@Test
+	public void testMsgShouldBeNull() {
+		TaskExecutionException ex = new TaskExecutionException();
+		assertNull(ex.getMessage());
+	}
+
+}

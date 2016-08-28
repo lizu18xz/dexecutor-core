7421b71c81671db2259a12ce392214c9a9fa1f85
Nadeem Mohammad <coolmind182006@gmail.com>
2016-08-28 23:02:27 +0530

    More tests

---
+++ src/test/java/com/github/dexecutor/executor/graph/NodeTest.java
@@ -0,0 +1,42 @@
+package com.github.dexecutor.executor.graph;
+
+import static org.junit.Assert.*;
+
+import static org.hamcrest.CoreMatchers.*;
+
+import org.junit.Test;
+
+import com.github.dexecutor.executor.graph.Graph.Node;
+
+public class NodeTest {
+
+	@Test
+	public void testNodeIsErrored() {
+		Node<Integer, Integer> node = new  Node<Integer, Integer>(1);
+		node.setErrored();
+		assertThat(node.isErrored(), equalTo(true));
+	}
+	
+	
+	@Test
+	public void testNodeIsNotErrored() {
+		Node<Integer, Integer> node = new  Node<Integer, Integer>(1);
+		node.setSuccess();
+		assertThat(node.isErrored(), equalTo(false));
+	}
+	
+	@Test
+	public void testNodeIsSkipped() {
+		Node<Integer, Integer> node = new  Node<Integer, Integer>(1);
+		node.setSkipped();
+		assertThat(node.isSkipped(), equalTo(true));
+	}
+	
+	@Test
+	public void testNodeIsNotSkipped() {
+		Node<Integer, Integer> node = new  Node<Integer, Integer>(1);
+		node.setErrored();
+		assertThat(node.isSkipped(), equalTo(false));
+	}
+
+}

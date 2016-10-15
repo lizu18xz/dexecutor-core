21c9f31885234e00b714c6f8d7481aaeb12cce24
Nadeem Mohammad <coolmind182006@gmail.com>
2016-10-15 17:03:08 +0530

    Refactored

---
+++ src/main/java/com/github/dexecutor/core/graph/BaseTraversar.java
@@ -0,0 +1,74 @@
+package com.github.dexecutor.core.graph;
+
+import java.io.IOException;
+import java.io.Writer;
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+import java.util.Set;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public abstract class BaseTraversar <T extends Comparable<T>, R> implements Traversar<T, R> {
+
+	private static final Logger logger = LoggerFactory.getLogger(BaseTraversar.class);
+
+	private static final String SPACE = " ";
+	private static final String EMPTY_STRING = "";
+
+	private List<Node<T, R>> processed = new ArrayList<Node<T, R>>();
+
+	protected List<List<List<Node<T, R>>>> traverseLevelOrder(final Dag<T, R> graph) {
+		List<List<List<Node<T, R>>>> result = new ArrayList<List<List<Node<T, R>>>>();
+		Set<Node<T, R>> initialNodes = graph.getInitialNodes();
+		for (Node<T, R> iNode : initialNodes) {
+			List<List<Node<T, R>>> iresult = new ArrayList<List<Node<T, R>>>();
+			doTraverse(iresult, iNode);
+			result.add(iresult);
+		}
+		return result;
+	}
+
+	private void doTraverse(final List<List<Node<T, R>>> result, final Node<T, R> iNode) {
+		Queue<Node<T, R>> queue = new LinkedList<Node<T, R>>();
+		queue.offer(iNode);
+		while (!queue.isEmpty()) {
+			List<Node<T, R>> level = new ArrayList<Node<T, R>>();
+			int size = queue.size();
+			for (int i = 0; i < size; i++) {
+				Node<T, R> node = queue.poll();
+				if (!this.processed.contains(node) && allProcessed(node.getInComingNodes())) {
+					if (!level.contains(node)) {
+						level.add(node);
+					}
+					this.processed.add(node);
+					for (Node<T, R> ogn : node.getOutGoingNodes()) {
+						if (ogn != null && !this.processed.contains(ogn)) {
+							queue.offer(ogn);
+						}
+					}
+				}				
+			}
+			result.add(level);
+		}
+	}
+
+	private boolean allProcessed(final Set<Node<T, R>> inComingNodes) {
+		return this.processed.containsAll(inComingNodes);
+	}
+
+	protected void printGraph(final List<List<Node<T, R>>> list, final Writer writer) {
+		for (List<Node<T, R>> nodes : list) {
+			try {
+				for (Node<T, R> node : nodes) {
+					writer.write(node + EMPTY_STRING + node.getInComingNodes() + SPACE);
+				}
+				writer.write("\n");
+			} catch (IOException e) {
+				logger.error("Error Writing ", e);
+			}
+		}
+	}
+}
--- src/main/java/com/github/dexecutor/core/graph/LevelOrderTraversar.java
+++ src/main/java/com/github/dexecutor/core/graph/LevelOrderTraversar.java
@@ -19,11 +19,7 @@ package com.github.dexecutor.core.graph;
 
 import java.io.IOException;
 import java.io.Writer;
-import java.util.ArrayList;
-import java.util.LinkedList;
 import java.util.List;
-import java.util.Queue;
-import java.util.Set;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -36,11 +32,9 @@ import org.slf4j.LoggerFactory;
  * @param <T> Type of Node/Task ID
  * @param <R> Type of Node/Task result
  */
-public class LevelOrderTraversar<T extends Comparable<T>, R> implements Traversar<T, R> {
+public class LevelOrderTraversar<T extends Comparable<T>, R> extends BaseTraversar<T, R> {
 
 	private static final Logger logger = LoggerFactory.getLogger(LevelOrderTraversar.class);
-
-	private List<Node<T, R>> processed = new ArrayList<Node<T, R>>();
 
 	public void traverse(final Dag<T, R> graph, final Writer writer) {
 		List<List<List<Node<T, R>>>> levelOrderOfGraphs = traverseLevelOrder(graph);
@@ -49,58 +43,6 @@ public class LevelOrderTraversar<T extends Comparable<T>, R> implements Traversa
 			try {
 				writer.write("Path #" + (i++) + "\n");
 				printGraph(levelOrderOfGraph, writer);
-				writer.write("\n");
-			} catch (IOException e) {
-				logger.error("Error Writing ", e);
-			}
-		}
-	}
-
-	private List<List<List<Node<T, R>>>> traverseLevelOrder(final Dag<T, R> graph) {
-		List<List<List<Node<T, R>>>> result = new ArrayList<List<List<Node<T, R>>>>();
-		Set<Node<T, R>> initialNodes = graph.getInitialNodes();
-		for (Node<T, R> iNode : initialNodes) {
-			List<List<Node<T, R>>> iresult = new ArrayList<List<Node<T, R>>>();
-			doTraverse(iresult, iNode);
-			result.add(iresult);
-		}
-		return result;
-	}
-
-	private void doTraverse(final List<List<Node<T, R>>> result, final Node<T, R> iNode) {
-		Queue<Node<T, R>> queue = new LinkedList<Node<T, R>>();
-		queue.offer(iNode);
-		while (!queue.isEmpty()) {
-			List<Node<T, R>> level = new ArrayList<Node<T, R>>();
-			int size = queue.size();
-			for (int i = 0; i < size; i++) {
-				Node<T, R> node = queue.poll();
-				if (!this.processed.contains(node) && allProcessed(node.getInComingNodes())) {
-					if (!level.contains(node)) {
-						level.add(node);
-					}
-					this.processed.add(node);
-					for (Node<T, R> ogn : node.getOutGoingNodes()) {
-						if (ogn != null && !this.processed.contains(ogn)) {
-							queue.offer(ogn);
-						}
-					}
-				}				
-			}
-			result.add(level);
-		}
-	}
-
-	private boolean allProcessed(Set<Node<T, R>> inComingNodes) {
-		return this.processed.containsAll(inComingNodes);
-	}
-
-	private void printGraph(final List<List<Node<T, R>>> list, final Writer writer) {
-		for (List<Node<T, R>> nodes : list) {
-			try {
-				for (Node<T, R> node : nodes) {
-					writer.write(node + "" + node.getInComingNodes() + " ");
-				}
 				writer.write("\n");
 			} catch (IOException e) {
 				logger.error("Error Writing ", e);
